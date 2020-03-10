35bf011f690ba5e4f06ce4f3a57f98d2b10bc406
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-03-10 10:44:01 -1000

    Completed up to part nine in class

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func guessNumberPressed(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Hello World", message: "this is my first alert view", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title:"Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        
+       print("the value of the slider is:\(slider.value)")
+    
+    currentValue = lroundf(slider.value)
 
+        
+    }
+    
+    
+    
 }
 
